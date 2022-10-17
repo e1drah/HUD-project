d7d8ef7d77c6925a7341fca35eee86e077fb63d9
e1drah <113189459+e1drah@users.noreply.github.com>
2022-10-17 12:37:56 -0300

    Added damage method, added life system, added shield regeneration method, added life lose method

--- HUD project/Program.cs
+++ HUD project/Program.cs
@@ -8,37 +8,43 @@ namespace HUD_project
 {
     internal class Program
     {
+
         //player stats
-        static string playerName;
+        static string playerName = "PlayerName";
         static int health;
         static int maxHealth;
-        static int sheild;
-        static int maxSheild;
+        static int shield;
+        static int maxShield;
         static int lives;
-        static int damage;
+        static int maxLives;
+        static int playerDamage;
         static int xp;
         static int xpToNextLevel;
         //monster stats
         static int MonsterHealth;
         static int MonsterDamage;
         static int MonsterSheild;
+
         static void Main(string[] args)
         {
             //Player starting stats
             health = 100;
             maxHealth = 100;
-            sheild = 100;
-            maxSheild = 100;
+            shield = 100;
+            maxShield = 100;
             xpToNextLevel = 1000;
             lives = 3;
-            playerName = "AAAAA";
+            maxLives = 99;
+            playerDamage = 10;
 
-            DebugMenu();
+            Showcase();
+            
         }
         //Debug menu to test methods
         static void DebugMenu()
         {
-            if (playerName == "AAAAA")
+            Console.Clear();
+            if (playerName == "")
             {
                 DeterminPlayerName();
             }
@@ -46,12 +52,73 @@ namespace HUD_project
             {
                 ShowHUD();
                 Console.WriteLine("Debug Commands");
-                Console.WriteLine("'Take damage'");
-                Console.WriteLine("'Heal self'");
-                Console.WriteLine("'Restore sheilds'");
-                Console.WriteLine("'Gain Experiance'");
+                Console.WriteLine("'Take playerDamage' - 1");
+                Console.WriteLine("'Heal self' - 2");
+                Console.WriteLine("'Restore sheilds' - 3");
+                Console.WriteLine("'Gain Experiance' - 4");
                 Console.ReadKey();
             }
+
+        }
+        static void Showcase()
+        {
+            //Showcases shield taking Damage
+            Console.WriteLine("Showcasing shield taking damage");
+            Console.WriteLine();
+            Reset();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            ShowHUD();
+            TakeDamage(50);
+            ShowHUD();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            Console.ReadKey();
+
+            //Showcases Sheild damage over flowing into health
+            Console.WriteLine("Showcasing sheild damage over flowing into health");
+            Console.WriteLine();
+            Reset();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            ShowHUD();
+            TakeDamage(150);
+            ShowHUD();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            Console.ReadKey();
+
+            //Showcases losing a life
+            Console.WriteLine("Showcasing losing a life");
+            Console.WriteLine();
+            Reset();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            ShowHUD();
+            TakeDamage(200);
+            ShowHUD();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            Console.ReadKey();
+
+            // Showcasing Player losing all lives
+            Console.WriteLine("Showcasing getting a game over");
+            Console.WriteLine();
+            Reset();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            ShowHUD();
+            TakeDamage(200);
+            ShowHUD();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            ShowHUD();
+            TakeDamage(200);
+            ShowHUD();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            ShowHUD();
+            TakeDamage(200);
+            ShowHUD();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            ShowHUD();
+            TakeDamage(200);
+            ShowHUD();
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            Console.ReadKey();
+
+            //
         }
         //Sets player name
         static void DeterminPlayerName()
@@ -59,10 +126,10 @@ namespace HUD_project
             Console.WriteLine("What is your name");
             Console.WriteLine();
             playerName = Console.ReadLine();
-            Console.Clear();
             DebugMenu();
 
         }
+        //heals player
         static void Heal(int healAmount)
         {
             health += healAmount;
@@ -70,13 +137,86 @@ namespace HUD_project
             {
                 health = 100;
             }
+            DebugMenu();
         }
-
+        static void RegenerateSheild(int regenerateAmount)
+        {
+            Console.WriteLine("shield will be regenarated by " + regenerateAmount + " points!");
+            if (regenerateAmount > 0)
+            {
+                shield += regenerateAmount;
+                if (shield > maxShield)
+                {
+                    shield = maxShield;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error! " + regenerateAmount + " is not a valid regenaration value! Player can not regenarate negative sheild");
+            }
+            Console.WriteLine("Player regenarates " + regenerateAmount + " points of shield");
+        }
+        //damages player
+        static void TakeDamage(int damage)
+        {
+            Console.WriteLine("Player is about to take " + damage + " points of damage!");
+            if (damage < 0)
+            {
+                Console.WriteLine("Error: " + damage + " is not a valid damage value! Player cannot take negative damage!");
+            }
+            else
+            {
+                if (shield >= 0)
+                {
+                    shield -= damage;
+                    if (shield < 0)
+                    {
+                        health -= (shield * -1);
+                        shield = 0;
+                        if (health <= 0)
+                        {
+                            health = 0;
+                            LoseALife();
+                        }
+                    }
+                }
+                Console.WriteLine(playerName + " takes " + damage + " damages");
+            }
+            Console.WriteLine();
+        }
+        //Takes away a life if player still has lives
+        static void LoseALife()
+        {
+            if (lives <= 0)
+            {
+                Console.WriteLine("Game over!");
+            }
+            else
+            {
+                health = maxHealth;
+                shield = maxShield;
+                lives -= 1;
+            }
+        }
+        static void Reset()
+        {
+            Console.WriteLine("Reseting varibles");
+            Console.WriteLine();
+            health = 100;
+            maxHealth = 100;
+            shield = 100;
+            maxShield = 100;
+            xpToNextLevel = 1000;
+            lives = 3;
+            maxLives = 99;
+            playerDamage = 10;
+        }
         //Shows HUD
         static void ShowHUD()
         {
-            Console.WriteLine(playerName + " Health: " + health + @"/" + maxHealth + " Sheild: " + sheild + @"/" + maxSheild + " XP: "+ xp + @"/" + xpToNextLevel);
+            Console.WriteLine(playerName + " Health: " + health + @"/" + maxHealth + " Shield: " + shield + @"/" + maxShield + " Lives: " + lives + " XP: "+ xp + @"/" + xpToNextLevel +" " + playerName + "'s Attack: " + playerDamage);
             Console.WriteLine();
         }
+
     }
 }
